d5daee4c8ca29f4ec4d4c1bce944ada225d0fa92
MichaelGofron <gofronm@yahoo.com>
2016-02-10 18:31:11 -0600

    (feat) added code to preprocess for adding header and generating output csv

--- Scripts/innocentiveMain.py
+++ Scripts/innocentiveMain.py
@@ -7,7 +7,6 @@ def makeTestOutput(modelfp, inTestFileName, outTestFileName):
   ML.generateTestOutput(dm.load(modelfp), dm.load(inTestFileName), outTestFileName)
 
 
-
-# dm.saveCSVasNP(ci.originalDataDirectory + "processed.csv")
-
-ML.generateTestOutput(dm.load(ci.modelsDirectory + "rForest_n_10.mod"), dm.load(ci.outputTestingDirectory + 'fullData_input.np'), ci.outputTestingDirectory + 'modelData_output.csv')
+#dm.insertFirstRow(ci.originalDataDirectory + "data_HIV_5class.csv")
+inData, outData = dm.preprocess(ci.originalDataDirectory + "data_HIV_5class_processed.csv")
+ML.generateTestOutput(dm.load(ci.modelsDirectory + "rForest_n_10.mod"), inData, ci.outputTestingDirectory + "testingOutput.csv")
