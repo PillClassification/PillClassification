5944d52dea1af0f6571afc33fbce5385a0fd2eb7
MichaelGofron <gofronm@yahoo.com>
2016-02-11 19:40:12 -0600

    (refactor) remove comments to run innocentiveMain

--- Scripts/innocentiveMain.py
+++ Scripts/innocentiveMain.py
@@ -8,13 +8,13 @@ def makeTestOutput(modelfp, inTestFileName, outTestFileName):
 
 
 #------- Preprocess and insert first row ---------------#
-#dm.insertFirstRow(ci.originalDataDirectory + "data_HIV_5class.csv")
+dm.insertFirstRow(ci.originalDataDirectory + "data_HIV_5class.csv")
 
 #--------- Data preprocess to python object and pickle object ------------#
-#inData, outData = dm.preprocess(ci.originalDataDirectory + "data_HIV_5class_processed.csv")
+inData, outData = dm.preprocess(ci.originalDataDirectory + "data_HIV_5class_processed.csv")
 
 #----------- Make Testing output with model and data ------------------#
-#ML.generateTestOutput(dm.load(ci.modelsDirectory + "rForest_n_10.mod"), inData, ci.outputTestingDirectory + "testingOutput.csv")
+ML.generateTestOutput(dm.load(ci.modelsDirectory + "rForest_n_10.mod"), inData, ci.outputTestingDirectory + "testingOutput.csv")
 
 #------------ Compare Score to actual data ------------------------------#
 print ML.percentCorrect(dm.load(ci.outputTestingDirectory + 'data_HIV_5class_processed_output.np'), dm.file2Data(ci.outputTestingDirectory + 'testingOutput.csv'))
